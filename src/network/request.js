8030b40cfcf61bd3f8bfaf284c6b9671903541f9
yu <840205028@qq.com>
2022-02-02 23:17:05 +0800

    小更

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 export function request(url, params) {
     const instance = axios.create({
         // baseURL: 'http://localhost:8080/api',
-        baseURL: '/api',
+        // baseURL: '/api',
+        baseURL: '/backend/',
         timeout: 30000,
         withCredentials: true,
     })
